06127ff8ca389c1d454ff4185b50ecadfe799ceb
julianjosesotogomez <julian.sotgomez@gmail.com>
2022-01-26 18:17:05 -0500

    Add project files.

---
+++ ApiFBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiFBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFBTarjeta
+{
+    public class AplicationDbContext: DbContext //DbContext crea una instancia en nuestra base de datos
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }//Mapear el modelo de la tabla de la base de datos 
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options)//Se instancia la clase
+        {
+
+        }
+    }
+}
---
+++ ApiFBTarjeta/Models/TarjetaCredito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFBTarjeta.Models
+{
+    public class TarjetaCredito
+    {
+
+        public int Id { get; set; }
+        [Required] //Me referencia que el campo no puede ser NULL
+        public string Titular { get; set; }
+        [Required] //Me referencia que el campo no puede ser NULL
+        public string NumeroTrajeta { get; set; }
+        [Required] //Me referencia que el campo no puede ser NULL
+        public string FechaDeExpiracion { get; set; }
+        [Required] //Me referencia que el campo no puede ser NULL
+        public string CVV { get; set; }
+    }
+}
